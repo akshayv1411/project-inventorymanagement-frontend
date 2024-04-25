b1feb05ce1858360fa7b038c4b4a6c356fb4ddb6
akshayv1411 <akshayv1411@gmail.com>
2024-04-25 09:05:29 -0400

    'newcommit'

---
+++ inventory_management/src/Componets/Login.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Avatar, FormControlLabel, Grid, Paper, TextField, Checkbox, Button, Typography, Link } from "@mui/material";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
+const Login = () => {
+  const paperStyle = {
+    padding: 20,
+    height: '70vh',
+    width: 280,
+    margin: "20px auto"
+  };
+
+  const avatarStyle = { backgroundColor: 'green' };
+
+  const textFieldStyle = {
+    marginBottom: '10px' 
+  };
+
+  return (
+    <Grid container justifyContent="center" alignItems="center">
+      <Paper elevation={10} style={paperStyle}>
+        <Grid align='center'>
+          <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
+          <h2>Sign in</h2>
+        </Grid> 
+        <Grid container direction="column" spacing={2} alignItems="center">
+          <Grid item>
+            <TextField label="Username" placeholder="Enter Username" fullWidth required style={textFieldStyle} />
+          </Grid>
+          <Grid item>
+            <TextField label="Password" placeholder="Enter Password" type="password" fullWidth required />
+          </Grid>
+          <Grid item>
+            <FormControlLabel
+              control={<Checkbox name="checkedB" color="primary" />}
+              label="Remember me"
+            />
+            <Button type="submit" variant="contained" color="primary" fullWidth>Sign In</Button>
+            <Typography>
+              <Link href="#" variant="body2">Forgot Password?</Link>
+            </Typography>
+            <Typography>
+              <Link href="#" variant="body2">Already have an account? Sign up</Link>
+            </Typography>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Grid>
+  );
+}
+
+export default Login;
